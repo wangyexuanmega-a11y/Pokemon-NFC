4c7a582c0b945e008632ab707f15b67e996eb325
NFC Buddy <prototype@nfcbuddy.dev>
2026-07-17 16:01:12 +0800

    Reduce model targetSize to 0.3 for better fit

--- js/creature.js
+++ js/creature.js
@@ -187,7 +187,7 @@ export function loadGLTF(url) {
       const box = new THREE.Box3().setFromObject(model);
       const size = box.getSize(new THREE.Vector3());
       const maxDim = Math.max(size.x, size.y, size.z);
-      const targetSize = 1.0;
+      const targetSize = 0.3;
       const scale = targetSize / maxDim;
       model.scale.set(scale, scale, scale);
 
